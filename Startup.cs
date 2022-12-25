c25cfb0abdac0b00f92a24cc0994c4877d175f05
TÜRKALP KUCUR <turkalp.kucur@gmail.com>
2022-12-26 01:57:16 +0300

    commit 4
    commit 4

--- Startup.cs
+++ Startup.cs
@@ -16,7 +16,7 @@ namespace select2dropdown2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,7 +28,7 @@ namespace select2dropdown2
             }
 
             app.UseRouting();
-
+            app.UseMvc();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
