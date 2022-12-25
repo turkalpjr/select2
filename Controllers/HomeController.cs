75f0698d5a7e8cd2c7a6f59ac85e0e65b84d507b
TÜRKALP KUCUR <turkalp.kucur@gmail.com>
2022-12-26 01:45:18 +0300

    commit#2
    commit#2

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace select2dropdown2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
